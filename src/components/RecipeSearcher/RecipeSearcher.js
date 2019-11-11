016fd1cb5f226d1bcf2fb4a07c4d8b5ae83ef97e
Matthew RIley <matthewriley@ctc_18512.barren.ketsds.net>
2019-11-11 14:06:46 -0600

    Added in the other two API calls and the default state.

--- src/components/RecipeSearcher/RecipeSearcher.js
+++ src/components/RecipeSearcher/RecipeSearcher.js
@@ -3,6 +3,11 @@ import axios from 'axios';
 
 class RecipeSearcher extends Component {
 
+    constructor(props){
+        super(props);
+        this.state = { recipes: [] }
+    }
+
     getRandomRecipe = () => {
         axios({
             // We can configure everything we need to about the http request in here
@@ -15,15 +20,58 @@ class RecipeSearcher extends Component {
             .catch(function (error) {
                 console.log(error);
             })
-}
+    }
 
-render() {
-    this.getRandomRecipe();
-    return (
-        <div className='RecipeSearcher'>
-        </div>
-    );
-}
+    getRecipeByName = (name) => {
+
+        axios({
+            // We can configure everything we need to about the http request in here
+            method: 'GET',
+            url: 'https://www.themealdb.com/api/json/v1/1/search.php',
+            params: {
+                s: name
+            }
+        })
+            .then(function (response) {
+                console.log(response);
+            })
+            .catch(function (error) {
+                console.log(error);
+            })
+    }
+
+
+    getRecipesByLetter = (letter) => {
+
+        axios({
+            // We can configure everything we need to about the http request in here
+            method: 'GET',
+            url: 'https://www.themealdb.com/api/json/v1/1/search.php',
+            params: {
+                f: letter
+            }
+        })
+            .then(function (response) {
+                console.log(response);
+            })
+            .catch(function (error) {
+                console.log(error);
+            })
+    }
+
+
+
+
+    render() {
+        // this.getRandomRecipe();
+        // this.getRecipeByName('Chicken');
+        this.getRecipesByLetter('M');
+
+        return (
+            <div className='RecipeSearcher'>
+            </div>
+        );
+    }
 }
 
 export default RecipeSearcher;
