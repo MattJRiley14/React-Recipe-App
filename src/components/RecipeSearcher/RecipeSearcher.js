61a4da7d4b511ad8428a17e97d3879a2e31783eb
Matthew RIley <matthewriley@ctc_18512.barren.ketsds.net>
2019-11-11 14:42:06 -0600

    Added functionality to update the state for RecipeSearcher whenever the API calls finish

--- src/components/RecipeSearcher/RecipeSearcher.js
+++ src/components/RecipeSearcher/RecipeSearcher.js
@@ -8,22 +8,33 @@ class RecipeSearcher extends Component {
         this.state = { recipes: [] }
     }
 
+    componentDidMount() {
+        // this.getRandomRecipe();
+        this.getRecipeByName('Roast');
+        // this.getRecipesByLetter('M');
+    }
+
     getRandomRecipe = () => {
+
+        const _this = this;
+        
         axios({
             // We can configure everything we need to about the http request in here
             method: 'GET',
             url: 'https://www.themealdb.com/api/json/v1/1/random.php'
         })
-            .then(function (response) {
-                console.log(response);
-            })
-            .catch(function (error) {
-                console.log(error);
-            })
+        .then(function (response) {
+            _this.setState({ recipes: response.data.meals || [] })
+        })
+        .catch(function (error) {
+            console.log(error);
+        })
     }
-
+    
     getRecipeByName = (name) => {
 
+        const _this = this;
+        
         axios({
             // We can configure everything we need to about the http request in here
             method: 'GET',
@@ -32,27 +43,29 @@ class RecipeSearcher extends Component {
                 s: name
             }
         })
-            .then(function (response) {
-                console.log(response);
-            })
-            .catch(function (error) {
-                console.log(error);
-            })
+        .then(function (response) {
+            _this.setState({ recipes: response.data.meals || [] })
+        })
+        .catch(function (error) {
+            console.log(error);
+        })
     }
-
-
+    
+    
     getRecipesByLetter = (letter) => {
 
-        axios({
-            // We can configure everything we need to about the http request in here
-            method: 'GET',
-            url: 'https://www.themealdb.com/api/json/v1/1/search.php',
-            params: {
-                f: letter
-            }
-        })
+        const _this = this;
+            
+            axios({
+                // We can configure everything we need to about the http request in here
+                method: 'GET',
+                url: 'https://www.themealdb.com/api/json/v1/1/search.php',
+                params: {
+                    f: letter
+                }
+            })
             .then(function (response) {
-                console.log(response);
+                _this.setState({ recipes: response.data.meals || [] })
             })
             .catch(function (error) {
                 console.log(error);
@@ -63,9 +76,6 @@ class RecipeSearcher extends Component {
 
 
     render() {
-        // this.getRandomRecipe();
-        // this.getRecipeByName('Chicken');
-        this.getRecipesByLetter('M');
 
         return (
             <div className='RecipeSearcher'>
