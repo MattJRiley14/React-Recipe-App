5acc4680eb6ec73f53f1fa5dea3f924f5b78c24b
Matthew RIley <matthewriley@Matthews-MacBook-Pro.local>
2019-11-13 15:54:31 -0600

    In RecipeSearcher.js: Set up firstLoad bool. So that it will allow display messages to appear at the correct times.

--- src/components/RecipeSearcher/RecipeSearcher.js
+++ src/components/RecipeSearcher/RecipeSearcher.js
@@ -5,9 +5,10 @@ import Header from '../Header/Header';
 
 class RecipeSearcher extends Component {
 
-    constructor(props){
+    constructor(props) {
         super(props);
         this.state = { recipes: [] }
+        this.state = { firstLoad: true }
     }
 
     componentDidMount() {
@@ -19,44 +20,46 @@ class RecipeSearcher extends Component {
     getRandomRecipe = () => {
 
         const _this = this;
-        
+
         axios({
             // We can configure everything we need to about the http request in here
             method: 'GET',
             url: 'https://www.themealdb.com/api/json/v1/1/random.php'
         })
-        .then(function (response) {
-            _this.setState({ recipes: response.data.meals || [] })
-        })
-        .catch(function (error) {
-            console.log(error);
-        })
-    }
-    
-    getRecipeByName = (name) => {
-
-        const _this = this;
+            .then(function (response) {
+                _this.setState({ recipes: response.data.meals || [] })
+                _this.setState({ firstLoad: false })
+            })
+            .catch(function (error) {
+                console.log(error);
+            })
+        }
         
-        axios({
-            // We can configure everything we need to about the http request in here
-            method: 'GET',
-            url: 'https://www.themealdb.com/api/json/v1/1/search.php',
-            params: {
-                s: name
-            }
-        })
-        .then(function (response) {
-            _this.setState({ recipes: response.data.meals || [] })
-        })
-        .catch(function (error) {
-            console.log(error);
-        })
-    }
-    
-    
-    getRecipesByLetter = (letter) => {
-
-        const _this = this;
+        getRecipeByName = (name) => {
+            
+            const _this = this;
+            
+            axios({
+                // We can configure everything we need to about the http request in here
+                method: 'GET',
+                url: 'https://www.themealdb.com/api/json/v1/1/search.php',
+                params: {
+                    s: name
+                }
+            })
+            .then(function (response) {
+                _this.setState({ recipes: response.data.meals || [] })
+                _this.setState({ firstLoad: false })
+            })
+            .catch(function (error) {
+                console.log(error);
+            })
+        }
+        
+        
+        getRecipesByLetter = (letter) => {
+            
+            const _this = this;
             
             axios({
                 // We can configure everything we need to about the http request in here
@@ -68,25 +71,23 @@ class RecipeSearcher extends Component {
             })
             .then(function (response) {
                 _this.setState({ recipes: response.data.meals || [] })
+                _this.setState({ firstLoad: false })
             })
             .catch(function (error) {
                 console.log(error);
             })
-    }
-
-
-
+        }
 
     render() {
 
         return (
             <div>
                 <Header
-                randomRecipeHandler={this.getRandomRecipe}
-                recipeByLetterHandler={this.getRecipesByLetter}
-                recipeByNameHandler={this.getRecipeByName}
-                 />
-                <RecipeList recipes={this.state.recipes} />
+                    randomRecipeHandler={this.getRandomRecipe}
+                    recipeByLetterHandler={this.getRecipesByLetter}
+                    recipeByNameHandler={this.getRecipeByName}
+                />
+                <RecipeList recipes={this.state.recipes} firstLoad={this.state.firstLoad} />
             </div>
         );
     }
